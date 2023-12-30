968c6d4b6dce90dc47eba26dd8e881ead277c8f0
Soulbolt <soulbolt@gmail.com>
2023-12-30 12:57:26 -0700

    implemented endpoints for CRUD operations with their respective required parameters.

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 from dotenv import load_dotenv
 import requests
+from datetime import datetime
 
 load_dotenv()
 PIXELA_ENDPOINT = "https://pixe.la/v1/users"
@@ -33,5 +34,25 @@ headers = {
     "X-USER-TOKEN": TOKEN
 }
 
-response = requests.post(url=GRAPH_ENDPOINT, json=graph_config, headers=headers)
+# Create Graph
+# response = requests.post(url=GRAPH_ENDPOINT, json=graph_config, headers=headers)
+# print(response.text)
+today = datetime.now()
+# Add a pixel to the graph
+ADD_PIXEL_ENDPOINT = f"{GRAPH_ENDPOINT}/graph1/add"
+add_pixel = {
+    "date": today.strftime("%Y%m%d"),
+    "quantity": "10",
+}
+# response =  requests.post(url=ADD_PIXEL_ENDPOINT, headers=headers, json=add_pixel)
+# print(response.text)
+update_pixel = {
+    "quantity": "20",
+}
+UPDATE_PIXEL_ENDPOINT = f"{GRAPH_ENDPOINT}/graph1/{today.strftime("%Y%m%d")}"
+# response = requests.put(url=UPDATE_PIXEL_ENDPOINT, headers=headers, json=update_pixel)
+# print(response.text)
+
+DELETE_PIXEL_ENDPOINT = f"{GRAPH_ENDPOINT}/graph1/{today.strftime("%Y%m%d")}"
+response = requests.delete(url=DELETE_PIXEL_ENDPOINT, headers=headers)
 print(response.text)
